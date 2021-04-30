c8965d2a4a303aa20db569c0ea19a286b572ad42
rkhanal <rkhanal@uno.edu>
2021-04-30 15:12:12 -0500

    Trying to deploy the current application to heroku

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const wsServer = new webSocketServer({
     httpServer: server
 });
 
-// Generates uniqueID for every new connection
+// Generates uniqueID for every new connectipm
 const getUniqueID = () => {
     const s4 = () => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
     return s4() + s4() + '-' + s4()
@@ -24,25 +24,14 @@ let boardState = null;
 // User activity history
 let userActivity = [];
 
-// Hold rooms below
-const rooms = {}
-
 //let currentUsers = null, userActivity = null , username = null, stateBoard = null, selectedPiece = null, turn = null, 
 //    takenWhitePieces = null, takenBlackPieces = null, win = null, check = null, whiteHasMoved = null, blackHasMoved = null;
 
-const sendMessage = (json, roomName) => {
-    // We are sending the current data to all connected clients in a particular room
-    if (json === '{"type":"userevent","data":{"error":"e"}}'){
-        console.log('This ??') //This is where we take care of the problem when a third person tries to enter a fully occupied room
-    }
-
-    let list_of_clients = rooms[roomName];
-    let i = 0;
-    if(list_of_clients){
-        for (i=0; i< list_of_clients.length; i++){
-            clients[list_of_clients[i]].sendUTF(json)
-        }
-    }
+const sendMessage = (json) => {
+    // We are sending the current data to all connected clients
+    Object.keys(clients).map((client) => {
+        clients[client].sendUTF(json);
+    });
 }
 
 const typesDef = {
@@ -67,78 +56,41 @@ wsServer.on('request', function(request) {
             const dataFromClient = JSON.parse(message.utf8Data);
             const json = { type: dataFromClient.type }
             
+            console.log("****************")
+            console.log(dataFromClient.type)
+            console.log("****************")
+
             // if the request from the client is a user event - received from the login information
             if (dataFromClient.type === typesDef.USER_EVENT){
                 
-                var test = true
-                var color = "white" // a player will be initially assigned a color 'White'
-
-                if (rooms[dataFromClient.username]){ // This if statement checks if there is a room already existing for a given username.
-                    if (rooms[dataFromClient.username].length == 2){
-                        test = false
-                        // we allow no additional players
-                        json.data = "e";
-                        delete clients[userID];
-                        connection.sendUTF(JSON.stringify(json));
-                    }else{
-                        // this should mean that the room was waiting for a second player and is now complete, the player coming second to the room should be assigned a "Black" color
-                        color = "black"
-                    }
-                }
-                if(test == true){
-
-                    // Somewhere within this if statement
-                    users[userID] = dataFromClient;
-                    userActivity.push(`${dataFromClient.playername} joined ${dataFromClient.username}`);
-                
-                    // Put all the users in a room together
-                    if (rooms[dataFromClient.username]){
-                        console.log((rooms[dataFromClient.username]).type)
-                        rooms[dataFromClient.username].push(userID)
-                    
-                    }else{
-                        rooms[dataFromClient.username] = [userID];
-                    }
-                    console.log(rooms[dataFromClient.username])
-                    
-                    json.data = {users, userActivity, color}
-                    console.log(json)
-                    sendMessage(JSON.stringify(json), dataFromClient.username);
-                }// end if test == True
+                users[userID] = dataFromClient;
+                userActivity.push(`${dataFromClient.username} joined`);
+                json.data = {users, userActivity}
             } 
             else if (dataFromClient.type === typesDef.CONTENT_CHANGE) {
                 
                 boardState = dataFromClient;
                 boardState.userActivity = userActivity;
                 json.data = {boardState};
-                console.log(json)
-                sendMessage(JSON.stringify(json), dataFromClient.username);
             }
+            console.log(json)
+            sendMessage(JSON.stringify(json));
         }
     });
     // user disconnected
     connection.on('close', function(connection){
-
-        // lets start by checking if the userid is in our records:
-        // if not no need to worry about it coz it was prolly a false alarm
+        
+        console.log((new Date()) + " Peer " + userID + " disconnected.");
+        //console.log(users[userID])
+        //console.log(users[userID].username)
         const json = { type: typesDef.USER_EVENT };
-
-        if(users[userID]){
-            
-            console.log('I will be called only some times')       
-            console.log((new Date()) + " Peer " + userID + " disconnected.");
-            console.log(`${users[userID].username} left`)
-            userActivity.push(`a player left ${users[userID].username}`);
-            // userActivity.push(`${dataFromClient.playername} left ${dataFromClient.username}`);
-
-            // Somewhere here - when both players leave a room - we can delete the room as a whole
-            // This will help us reuse the rooms for other playes
-
-            json.data = { users, userActivity };
-            delete clients[userID];
-            delete users[userID];
-
-            sendMessage(JSON.stringify.json);
-        }
+        //console.log('1')
+        userActivity.push(`${users[userID].username} left`);
+        //console.log('2')
+        json.data = { users, userActivity };
+        //console.log('3')
+        delete clients[userID];
+        delete users[userID];
+        sendMessage(JSON.stringify.json);
     });
 });
