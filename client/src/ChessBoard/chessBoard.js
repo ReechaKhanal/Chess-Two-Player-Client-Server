956691d1f0955e86cd1108359ebf3759ef4c9951
rkhanal <rkhanal@uno.edu>
2021-05-03 15:41:17 -0500

    Trying to deploy

--- client/src/ChessBoard/chessBoard.js
+++ client/src/ChessBoard/chessBoard.js
@@ -11,6 +11,7 @@ import { getBoardFromStateBoard } from "./getStateBoard";
 import { getAllLegalMoves } from "./legalMoves/getLegalMoves";
 import { w3cwebsocket as W3CWebSocket } from "websocket";
 import Identicon from 'react-identicons';
+
 const client = new W3CWebSocket('ws://127.0.0.1:8000');
 
 class ChessBoard extends Component{
