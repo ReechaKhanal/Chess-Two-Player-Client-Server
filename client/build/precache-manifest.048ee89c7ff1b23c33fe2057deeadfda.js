d8be6eb5a3134f5f54d603a60ab1b9827f9087d9
rkhanal <rkhanal@uno.edu>
2021-05-03 13:45:02 -0500

    Effort in Deployment

---
+++ client/build/precache-manifest.048ee89c7ff1b23c33fe2057deeadfda.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "dba9cae9b4264d7dd563",
+    "url": "/static/css/main.f297e7a8.chunk.css"
+  },
+  {
+    "revision": "dba9cae9b4264d7dd563",
+    "url": "/static/js/main.b0414c5e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "219435d3eb21e4535908",
+    "url": "/static/js/2.47014c07.chunk.js"
+  },
+  {
+    "revision": "4fe7a402c29b62e8e2667ebdfa193652",
+    "url": "/static/media/black_pawn.4fe7a402.png"
+  },
+  {
+    "revision": "dbebd7847df2a214fbddd4ee720b1fbe",
+    "url": "/static/media/pawn.dbebd784.png"
+  },
+  {
+    "revision": "96ec84cb0282d190de7efdf908b959a1",
+    "url": "/static/media/knight.96ec84cb.png"
+  },
+  {
+    "revision": "421af743118f9d1c63a6380a96d4da04",
+    "url": "/static/media/black_knight.421af743.png"
+  },
+  {
+    "revision": "19622d53f8de8b98fef1cf80a677dee0",
+    "url": "/static/media/bishop.19622d53.png"
+  },
+  {
+    "revision": "191a59b8cc2a1f62094471f8adf9146e",
+    "url": "/static/media/queen.191a59b8.png"
+  },
+  {
+    "revision": "86f329264d0493dc9d1335a23fad28a0",
+    "url": "/static/media/black_queen.86f32926.png"
+  },
+  {
+    "revision": "b5a4c1b03bfd0bac66f91c2e8482a070",
+    "url": "/static/media/empty.b5a4c1b0.svg"
+  },
+  {
+    "revision": "cc841b03a01a593a126d988288d4db4f",
+    "url": "/index.html"
+  }
+];
