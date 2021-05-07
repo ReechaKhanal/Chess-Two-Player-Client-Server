4583e2fb5f798a844b46f153d69876cb15f10254
rkhanal <rkhanal@uno.edu>
2021-05-07 15:13:01 -0500

    Changed client URL to see if deployment works now

--- client/src/ChessBoard/chessBoard.js
+++ client/src/ChessBoard/chessBoard.js
@@ -12,12 +12,11 @@ import { getAllLegalMoves } from "./legalMoves/getLegalMoves";
 import { w3cwebsocket as W3CWebSocket } from "websocket";
 import Identicon from 'react-identicons';
 
-
-
-
 const webSocketsServerPort = process.env.PORT || 8000;
 //const client = new W3CWebSocket('ws://127.0.0.1:8000');
-const client = new W3CWebSocket('ws://127.0.0.1:'+webSocketsServerPort);
+//const client = new W3CWebSocket('ws://127.0.0.1:'+webSocketsServerPort);
+const url = "https://chess-two-player.herokuapp.com/";
+const client = new W3CWebSocket(url);
 
 class ChessBoard extends Component{
 
