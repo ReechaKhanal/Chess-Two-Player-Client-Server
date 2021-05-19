7033e324a0cbd2b4c26c9f3f1bd35ad96729e121
rkhanal <rkhanal@uno.edu>
2021-05-18 22:56:14 -0700

    Trying out socket.io

--- client/src/App.js
+++ client/src/App.js
@@ -1,51 +1,14 @@
-// import React from "react";
-import logo from './logo.svg';
-import './App.css';
-import ReactDOM from 'react-dom';
-import ChessBoard from "./ChessBoard/chessBoard";
+import React from 'react';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
 
-import React, { Component } from 'react';
-import { w3cwebsocket as W3CWebSocket } from "websocket";
+import Join from './components/Join';
+import Play from './components/Play';
 
-//const client = new W3CWebSocket('ws://127.0.0.1:8000');
-const client = new W3CWebSocket('ws://chess-two-player.herokuapp.com/');
+const App = () => (
+    <Router>
+        <Route path="/" exact component={Join}/>
+        <Route path="/play" exact component={Play}/>
+    </Router>
+);
 
-class App extends Component {
-  componentWillMount() {
-    client.onopen = () => {
-      console.log('WebSocket Client Connected');
-    };
-    client.onmessage = (message) => {
-      console.log(message);
-    };
-  }
-  
-  render() {
-    return (
-      <div>
-        Practical Intro To WebSockets.
-      </div>
-    );
-  }
-}
-
-/*
-function App() {
-  const [data, setData] = React.useState(null);
-
-  React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
-
- return (
-   <div>
-     <h1>Hey There</h1>
-     <h2> If you see me I work ! </h2>
-   </div>
- );
-
-}
-*/
 export default App;
