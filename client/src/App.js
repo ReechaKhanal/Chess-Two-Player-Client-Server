2e0546f545c7cdefed68f77a4db0d57749348f27
rkhanal <rkhanal@uno.edu>
2021-05-19 14:49:53 -0700

    Finally a version that works!!

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,46 @@
+/* eslint-disable */ 
 import React from 'react';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import ReactDOM from 'react-dom';
+import { router } from 'websocket';
 
-import Join from './components/Join';
-import Play from './components/Play';
+import * as serviceWorker from './serviceWorker';
 
-const App = () => (
-    <Router>
-        <Route path="/" exact component={Join}/>
-        <Route path="/play" exact component={Play}/>
-    </Router>
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import ChessBoard from "./ChessBoard/chessBoard";
+
+/*
+ReactDOM.render(
+    <App />, document.getElementById('root')
 );
+*/
+/*
+ReactDOM.render(
+  
+    <div class = "chess">
+      <h1>Reecha's Chess Board</h1>
+      <ChessBoard/>
+    </div>,
+    document.getElementById('root')
+  
+    <div class = "chess">
+      <h1>Reecha's Chess Board</h1>
+      
+    </div>
+  );
+
+  export default App;
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
+*/
+
+const App = () => {
+  return (
+    <Router>
+      <Route path = "/" exact component={ChessBoard}/>
+    </Router>
+  );
+}
 
 export default App;
